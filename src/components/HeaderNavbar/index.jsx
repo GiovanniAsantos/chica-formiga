d3eeb255546ff75940e09a0d7491b992e80c749b
Giovanni Azevedo <giovanniadev@gmail.com>
2025-02-11 17:07:00 -0300

    ajuste no handlescroll

--- src/components/HeaderNavbar/index.jsx
+++ src/components/HeaderNavbar/index.jsx
@@ -2,20 +2,42 @@ import PropTypes from "prop-types";
 import "./style.css";
 
 const HeaderNavbar = ({ heroRef, aboutUsRef, productsRef, clientsRef }) => {
-  return <>
-  <div className="navbar">
-        <button className="buttonNavbar" onClick={() => handleScroll(heroRef)}>Home</button>
-        <button className="buttonNavbar" onClick={() => handleScroll(aboutUsRef)}>Sobre</button>
-        <button className="buttonNavbar" onClick={() => handleScroll(clientsRef)}>Clientes</button>
-        <button className="buttonNavbar" onClick={() => handleScroll(productsRef)}>Produtos</button>
-      </div >
-</>;
+    const handleScroll = (ref) => {
+        ref.current?.scrollIntoView({ behavior: "smooth" });
+      };
+  return (
+    <>
+      <div className="navbar">
+        <button className="buttonNavbar" onClick={() => handleScroll(heroRef)}>
+          Home
+        </button>
+        <button
+          className="buttonNavbar"
+          onClick={() => handleScroll(aboutUsRef)}
+        >
+          Sobre
+        </button>
+        <button
+          className="buttonNavbar"
+          onClick={() => handleScroll(clientsRef)}
+        >
+          Clientes
+        </button>
+        <button
+          className="buttonNavbar"
+          onClick={() => handleScroll(productsRef)}
+        >
+          Produtos
+        </button>
+      </div>
+    </>
+  );
 };
 HeaderNavbar.propTypes = {
-    heroRef: PropTypes.object.isRequired,
-    aboutUsRef: PropTypes.object.isRequired,
-    productsRef: PropTypes.object.isRequired,
-    clientsRef: PropTypes.object.isRequired,
-  };
-  
+  heroRef: PropTypes.object.isRequired,
+  aboutUsRef: PropTypes.object.isRequired,
+  productsRef: PropTypes.object.isRequired,
+  clientsRef: PropTypes.object.isRequired,
+};
+
 export default HeaderNavbar;
