1345defcfb7907f1bedffb28b721c1d31f0730f1
Giovanni Azevedo <giovanniadev@gmail.com>
2025-02-11 17:58:18 -0300

    tentativa de tirar a cor azul

--- src/components/HeaderNavbar/index.jsx
+++ src/components/HeaderNavbar/index.jsx
@@ -1,36 +1,26 @@
-import { useRef } from "react";
+import PropTypes from "prop-types";
+import "./style.css";
 
 const HeaderNavbar = ({ heroRef, aboutUsRef, productsRef, clientsRef }) => {
-  const buttonsRef = useRef([]);
-
-  const handleScroll = (ref, index) => {
+  const handleScroll = (ref) => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
-    
-    // Remove o foco do botão após clicar
-    if (buttonsRef.current[index]) {
-      buttonsRef.current[index].blur();
-    }
   };
 
   return (
     <div className="navbar">
-      {[
-        { label: "Home", ref: heroRef },
-        { label: "Sobre", ref: aboutUsRef },
-        { label: "Clientes", ref: clientsRef },
-        { label: "Produtos", ref: productsRef },
-      ].map((item, index) => (
-        <button
-          key={index}
-          className="buttonNavbar"
-          ref={(el) => (buttonsRef.current[index] = el)}
-          onClick={() => handleScroll(item.ref, index)}
-        >
-          <span className="buttonLabel">{item.label}</span>
-        </button>
-      ))}
+      <span className="navItem" onClick={() => handleScroll(heroRef)}>Home</span>
+      <span className="navItem" onClick={() => handleScroll(aboutUsRef)}>Sobre</span>
+      <span className="navItem" onClick={() => handleScroll(clientsRef)}>Clientes</span>
+      <span className="navItem" onClick={() => handleScroll(productsRef)}>Produtos</span>
     </div>
   );
+};
+
+HeaderNavbar.propTypes = {
+  heroRef: PropTypes.object.isRequired,
+  aboutUsRef: PropTypes.object.isRequired,
+  productsRef: PropTypes.object.isRequired,
+  clientsRef: PropTypes.object.isRequired,
 };
 
 export default HeaderNavbar;
