5647a40d197d1fd7f19d201a4683734a5fcb4a67
Giovanni <giovanniadev@gmail.com>
2025-02-13 09:34:58 -0300

    tentativa de subir 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      css: {
+        // Configuração adicional de CSS pode ser feita aqui, se necessário
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['antd/dist/antd.css'] // Adiciona o CSS do Ant Design como dependência externa
+    }
+  }
+});
