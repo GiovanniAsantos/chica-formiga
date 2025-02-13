e009fca1122b20d243ce60a592edf1ed3efa4b51
Giovanni <giovanniadev@gmail.com>
2025-02-13 09:30:40 -0300

    tentativa de deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'antd/dist/antd.css';
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
